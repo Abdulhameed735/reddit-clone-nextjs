60c385cdaba15cc7f6164fc2d4bcc0246379f842
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-07-13 22:08:02 +0100

    trending component changes - still working

--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -1,21 +1,43 @@
 import React from "react";
 import Image from "next/image";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { trending } from "@/data/trending";
 
 const Trending = () => {
   return (
     <section className="absolute w-full lg:w-[75%] h-screen right-0 p-3 lg:p-8">
       <div className="flex flex-col gap-2">
-        <h1>Trending</h1>
-        <div className="w-full md:w-[250px] lg:w-[270px]">
-          <AspectRatio ratio={16 / 9} className="bg-muted">
-            <Image
-              src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-              alt="Photo by Drew Beamer"
-              fill
-              className="rounded-md object-cover"
-            />
-          </AspectRatio>
+        <h1>Trending now</h1>
+        <div className="flex gap-2 relative">
+          {trending.map((trend) => (
+            <div key={trend.id} className="w-full md:w-[250px] lg:w-[270px]">
+              <AspectRatio
+                ratio={16 / 9}
+                className="bg-muted shadow-lg relative"
+              >
+                <Image
+                  src={trend.image}
+                  alt="Photo by Drew Beamer"
+                  fill
+                  className="rounded-md object-cover"
+                />
+              </AspectRatio>
+              <div className="absolute bottom-0 p-1 bg-black bg-opacity-50 text-white">
+                <h2 className="text-[18px] font-bold">{trend.title}</h2>
+                <p className="text-sm">{trend.description}</p>
+                <div className="flex items-center gap-1">
+                  <Avatar className="w-5 h-5">
+                    <AvatarImage src={trend.avatar} alt="avatar" />
+                    <AvatarFallback>{trend.initials}</AvatarFallback>
+                  </Avatar>
+                  <span className="text-xs font-semibold">
+                    {trend.community}
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
---
+++ src/data/trending.tsx
@@ -0,0 +1,46 @@
+export const trending = [
+  {
+    id: 1,
+    title: "LeBron james not retiring",
+    description: "Lakers LeBron James confirms return for 21st NBA season",
+    image:
+      "https://external-preview.redd.it/xlwWCFaWYaFvXY43lZgod4dnQhruHaoYC3G0Hqfeqxc.jpg?auto=webp&s=fb5090e51e10fab720ddfad4b0c233c00de9be87",
+    avatar:
+      "https://styles.redditmedia.com/t5_2qgzy/styles/communityIcon_rvt3zjh1fc551.png?width=256&s=87c202e9000d8061f18a9d72b106bdc9c8a219cc",
+    initials: "AR",
+    community: "r/sports and more",
+  },
+  {
+    id: 2,
+    title: "LeBron james not retiring",
+    description: "Lakers LeBron James confirms return for 21st NBA season",
+    image:
+      "https://external-preview.redd.it/xlwWCFaWYaFvXY43lZgod4dnQhruHaoYC3G0Hqfeqxc.jpg?auto=webp&s=fb5090e51e10fab720ddfad4b0c233c00de9be87",
+    avatar:
+      "https://styles.redditmedia.com/t5_2qgzy/styles/communityIcon_rvt3zjh1fc551.png?width=256&s=87c202e9000d8061f18a9d72b106bdc9c8a219cc",
+    initials: "AR",
+    community: "r/sports and more",
+  },
+  {
+    id: 3,
+    title: "LeBron james not retiring",
+    description: "Lakers LeBron James confirms return for 21st NBA season",
+    image:
+      "https://external-preview.redd.it/xlwWCFaWYaFvXY43lZgod4dnQhruHaoYC3G0Hqfeqxc.jpg?auto=webp&s=fb5090e51e10fab720ddfad4b0c233c00de9be87",
+    avatar:
+      "https://styles.redditmedia.com/t5_2qgzy/styles/communityIcon_rvt3zjh1fc551.png?width=256&s=87c202e9000d8061f18a9d72b106bdc9c8a219cc",
+    initials: "AR",
+    community: "r/sports and more",
+  },
+  {
+    id: 4,
+    title: "LeBron james not retiring",
+    description: "Lakers LeBron James confirms return for 21st NBA season",
+    image:
+      "https://external-preview.redd.it/xlwWCFaWYaFvXY43lZgod4dnQhruHaoYC3G0Hqfeqxc.jpg?auto=webp&s=fb5090e51e10fab720ddfad4b0c233c00de9be87",
+    avatar:
+      "https://styles.redditmedia.com/t5_2qgzy/styles/communityIcon_rvt3zjh1fc551.png?width=256&s=87c202e9000d8061f18a9d72b106bdc9c8a219cc",
+    initials: "AR",
+    community: "r/sports and more",
+  },
+];
