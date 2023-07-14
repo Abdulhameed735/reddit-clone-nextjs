77ab567506dafd5d535ab12f3cbd5425b97e9b4b
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-07-14 22:58:51 +0100

    Trending component completed

--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -2,11 +2,38 @@ import React from "react";
 import Image from "next/image";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { trending } from "@/data/trending";
+import Link from "next/link";
 
 const Trending = () => {
   return (
-    <div className="">
-      <h1>Hey</h1>
+    <div className="flex flex-col gap-2">
+      <h1 className="text-sm text-black dark:text-[#dad4d6]">Trending today</h1>
+      <div className="flex flex-1 gap-3 w-full overflow-x-scroll scrollbar-none">
+        {trending.map((trend) => (
+          <Link key={trend.id} href="/">
+            <div className="relative w-[250px] h-[170px] min-w-[210px] max-w-[375px] lg:w-[230px] lg:h-[150px] rounded-lg shadow-xl">
+              <Image
+                src={trend.image}
+                alt="Image"
+                className="w-full h-full object-cover rounded-lg brightness-50"
+                fill
+              />
+
+              <div className="absolute bottom-0 left-0 right-0 p-2 mt-10 text-white">
+                <h1 className="text-base font-semibold">{trend.title}</h1>
+                <p className="text-sm line-clamp-2">{trend.description}</p>
+
+                <div className="flex items-center gap-2 mt-3">
+                  <Avatar className="w-5 h-5">
+                    <AvatarImage src={trend.avatar} alt="avatar" />
+                  </Avatar>
+                  <p className="text-xs">{trend.community}</p>
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
--- src/data/trending.tsx
+++ src/data/trending.tsx
@@ -12,35 +12,37 @@ export const trending = [
   },
   {
     id: 2,
-    title: "LeBron james not retiring",
-    description: "Lakers LeBron James confirms return for 21st NBA season",
+    title: "Student debt relief of $39B",
+    description:
+      "Biden administration forgives $39 billion in student debt for more than 800,000 borrowers",
     image:
-      "https://external-preview.redd.it/xlwWCFaWYaFvXY43lZgod4dnQhruHaoYC3G0Hqfeqxc.jpg?auto=webp&s=fb5090e51e10fab720ddfad4b0c233c00de9be87",
+      "https://external-preview.redd.it/4ehq3b11b1epfmVppFsX92JsSztRlrP4irEfroKlbEo.jpg?auto=webp&s=f15b1a0ef282162b75dd36c3821fc90d63fb5a3e",
     avatar:
-      "https://styles.redditmedia.com/t5_2qgzy/styles/communityIcon_rvt3zjh1fc551.png?width=256&s=87c202e9000d8061f18a9d72b106bdc9c8a219cc",
+      "https://styles.redditmedia.com/t5_2qhpn/styles/communityIcon_66edptlscfo81.png?width=256&s=1be36aa8609b08a686fc9858db43d340ec42eb10",
     initials: "AR",
-    community: "r/sports and more",
+    community: "r/economy and more",
   },
   {
     id: 3,
-    title: "LeBron james not retiring",
-    description: "Lakers LeBron James confirms return for 21st NBA season",
+    title: "Harry Kane transfer talks",
+    description:
+      "[Dan Kilpatrick] Harry Kane does not intend to agitate to leave Spurs this summer, even if the club continues to rebuff bids from Bayern Munich. If he is to go, Kane is determined to do so on positive terms, also meaning he would not entertain a move to Chelsea.",
     image:
-      "https://external-preview.redd.it/xlwWCFaWYaFvXY43lZgod4dnQhruHaoYC3G0Hqfeqxc.jpg?auto=webp&s=fb5090e51e10fab720ddfad4b0c233c00de9be87",
+      "https://external-preview.redd.it/ZmDbmL5U0lHuP-VO2jIEe9LQQ6SEBoQFuYHkoj6Cpg0.jpg?auto=webp&s=4138f54affe9c70e5f1be44bcf6d789725c1997e",
     avatar:
-      "https://styles.redditmedia.com/t5_2qgzy/styles/communityIcon_rvt3zjh1fc551.png?width=256&s=87c202e9000d8061f18a9d72b106bdc9c8a219cc",
+      "https://styles.redditmedia.com/t5_2ryq7/styles/communityIcon_8kaqbft33ze91.png?width=256&s=574a872ccba14b9becdb8ed3bb225e9d1c255d22",
     initials: "AR",
     community: "r/sports and more",
   },
   {
     id: 4,
-    title: "LeBron james not retiring",
-    description: "Lakers LeBron James confirms return for 21st NBA season",
+    title: "India launches moon rocket",
+    description: "ISRO successfully launched Chandrayaan-3 Moon Mission.",
     image:
-      "https://external-preview.redd.it/xlwWCFaWYaFvXY43lZgod4dnQhruHaoYC3G0Hqfeqxc.jpg?auto=webp&s=fb5090e51e10fab720ddfad4b0c233c00de9be87",
+      "https://external-preview.redd.it/DAFxN4Zy0RnHlLfwpvSBJ7YTvg3NhHyERURYa2GmMTc.png?format=pjpg&auto=webp&s=de4310b214e10b7b54da2b962039fdd8ffa89271",
     avatar:
-      "https://styles.redditmedia.com/t5_2qgzy/styles/communityIcon_rvt3zjh1fc551.png?width=256&s=87c202e9000d8061f18a9d72b106bdc9c8a219cc",
+      "https://styles.redditmedia.com/t5_2qh87/styles/communityIcon_ub69d1lpjlf51.png?width=256&s=920c352b6d0c69518b6978ba8b456176a8d63c25",
     initials: "AR",
-    community: "r/sports and more",
+    community: "r/space and more",
   },
 ];
